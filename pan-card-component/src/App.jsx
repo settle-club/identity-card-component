bb26fd870ffbda920392646da31598fe908aa381
Rithik <jainrithik2000@gmail.com>
2024-06-13 14:33:45 +0530

    Add PAN card component

---
+++ pan-card-component/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PanCard from "./PanCard";
+
+function App() {
+  return (
+    <div className="App">
+      <PanCard name="PRANAV SATYAWAN MAHADIK" panNumber="CXSPM6036K" />
+    </div>
+  );
+}
+
+export default App;
