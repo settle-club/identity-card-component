3c9a838817aacb18c00e4eeaa158b8c60038b636
Rithik <jainrithik2000@gmail.com>
2024-06-20 14:30:51 +0530

    added swipe feature

--- src/AadhaarCard.jsx
+++ src/AadhaarCard.jsx
@@ -7,7 +7,12 @@ function AadhaarCard({name, dob, gender, image, number, address, area, district,
   const [isFlipped, setIsFlipped] = useState(false);
   const [flipDirection, setFlipDirection] = useState('right');
 
-  const handleFlip = (e) => {
+  const handleFlip = (direction) => {
+    setFlipDirection(direction);
+    setIsFlipped(!isFlipped);
+  }
+
+  const handleClick = (e) => {
     const cardWidth = e.currentTarget.offsetWidth;
     const clickX = e.clientX - e.currentTarget.getBoundingClientRect().left;
 
@@ -20,9 +25,26 @@ function AadhaarCard({name, dob, gender, image, number, address, area, district,
     setIsFlipped(!isFlipped);
   };
 
+  const handlePaginationClick = (isFront) => {
+    if(isFront && isFlipped) {
+      setFlipDirection('left');
+      setIsFlipped(false);
+    } else if (!isFront && !isFlipped) {
+      setFlipDirection('right');
+      setIsFlipped(true);
+    }
+  }
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => handleFlip('left'),
+    onSwipedRight: () => handleFlip('right'),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  })
+
   return (
     <div className="AadhaarApp">
-      <div className={`card-container`} onClick={handleFlip}>
+      <div className={`card-container`} onClick={handleClick} {...swipeHandlers}>
         <div className={`Aadhaarcard ${isFlipped ? `flipped-${flipDirection}` : ''}`}>
           <div className="front">
             <AadhaarCardFront name={name} dob={dob} gender={gender} image={image} number={number} />
@@ -33,8 +55,8 @@ function AadhaarCard({name, dob, gender, image, number, address, area, district,
         </div>
       </div>
       <div className="pagination">
-        <div className={`circle ${!isFlipped ? 'active' : ''}`} style={{backgroundColor: !isFlipped ? color : '#ccc'}}></div>
-        <div className={`circle ${isFlipped ? 'active' : ''}`} style={{backgroundColor: isFlipped ? color : '#ccc'}}></div>
+        <div className={`circle ${!isFlipped ? 'active' : ''}`} style={{backgroundColor: !isFlipped ? color : '#ccc'}} onClick={() => handlePaginationClick(true)}></div>
+        <div className={`circle ${isFlipped ? 'active' : ''}`} style={{backgroundColor: isFlipped ? color : '#ccc'}} onClick={() => handlePaginationClick(false)}></div>
       </div>
     </div>
   );
